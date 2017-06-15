18679a5e97f6e8ad1e609f1a5d2899612495c536
Iestyn Best <vfreeze_31@hotmail.com>
2017-06-15 16:12:50 +1000

    Updated App2

--- apps/02-guess-number-app/you_try/program.py
+++ apps/02-guess-number-app/you_try/program.py
@@ -5,6 +5,8 @@ print('      NUMBER GUESS GAME')
 print('-------------------------------')
 print()
 
+name = input('What is your name? ')
+print()
 the_number = random.randint(0,100)
 guess = -1
 
@@ -12,11 +14,11 @@ while guess != the_number:
     guess = int(input('Guess a number between 0 and 100: '))
 
     if guess < the_number:
-        print('too low')
+        print('Sorry {}, your guess of {} is too LOW'.format(name, guess))
+        print()
     elif guess > the_number:
-        print('too high')
+        print('Sorry {}, your guess of {} is too HIGH'.format(name, guess))
+        print()
     else:
-        print('You WIN!!')
-
-print()
-print('DONE...')
+        print()
+        print('{} is correct. You WIN {}!!'.format(guess, name))
